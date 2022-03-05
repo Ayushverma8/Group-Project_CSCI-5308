0c47e121f92667e785cec910aedcfa17dd621397
Manasvi Sharma <manasvisharma@Manasvis-MacBook-Air.local>
2022-03-05 14:00:59 -0400

    solve coflict

--- password_vault_fronend/src/App.js
+++ password_vault_fronend/src/App.js
@@ -10,7 +10,6 @@ import ForgetPassword from './components/ForgetPassword';
 import ResetPassword from './components/ResetPassword';
 import Notes from './components/Notes'
 import  AddNotes  from "./components/AddNotes";
-//import "bootstrap/dist/css/bootstrap.min.css";
 import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
