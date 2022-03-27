a8c86c2467ff3c7d57ab99eb24b59da4751cdac8
Deep Adeshra <dp974154@dal.ca>
2022-03-27 14:08:43 -0300

    Changes to make things run on prod.

--- password_vault_backend/password_vault_backend/settings.py
+++ password_vault_backend/password_vault_backend/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # rest framework config
 DEFAULT_RENDERER_CLASSES = (
