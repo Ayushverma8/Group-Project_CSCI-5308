d4e6f8411fa1e1b68f98a39c9aea5d710cad4e46
Ayush Verma <ay322266@dal.ca>
2022-03-27 16:22:10 -0300

    Added nohup in the Gunicorn import

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -3,5 +3,6 @@ chmod -R 777 /usr/share/nginx/html/
 cd /usr/share/nginx/html/
 virtualenv asdc-group5
 source asdc-group5/bin/activate
-python3 password_vault_backend/manage.py collectstatic --noinput
-#nohup python3 password_vault_backend/manage.py runserver 0.0.0.0:8000 &
+pip install -r requirements
+python3 password_vault_backend/manage.py  collectstatic --noinput
+nohup python3 password_vault_backend/manage.py runserver 0.0.0.0:8000  >/dev/null 2>&1 &
