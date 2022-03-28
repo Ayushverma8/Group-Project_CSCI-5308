153d727df2b33e941b381d91e91b51a0b7d18c48
Ayush Verma <ay322266@dal.ca>
2022-03-28 17:28:03 -0300

    Added tests for methods in the User Serializer

--- password_vault_backend/users/tests.py
+++ password_vault_backend/users/tests.py
@@ -1,9 +1,10 @@
 from unittest.mock import Mock
 
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-
+import mock
 from .serializers import *
-
+from unittest.mock import MagicMock
 
 class UserProfileAbstractSerializerTestCase(TestCase):
     """
@@ -42,3 +43,14 @@ class UserProfileAbstractSerializerTestCase(TestCase):
         instantiated_profile_class = UserProfileAbstractSerializer()
         t = instantiated_profile_class.validate_last_name("Verma@")
         self.assertNotEqual(t, "Verma")
+
+    def test_validate_email_if_email_does_not_exist(self):
+        instantiated_profile_class = UserProfileAbstractSerializer()
+        t = instantiated_profile_class.validate_email("ayush.verma@dal.ca")
+        self.assertEqual(t, "ayush.verma@dal.ca")
+
+    def test_validate_email_if_email_exist(self):
+        instantiated_profile_class = UserProfileAbstractSerializer()
+        instantiated_profile_class.validate_email = MagicMock(side_effect=ValidationError('Account with this email '
+                                                                                          'already exists'))
+        self.assertRaises(ValidationError, instantiated_profile_class.validate_email, "dp974154@dal.ca")
