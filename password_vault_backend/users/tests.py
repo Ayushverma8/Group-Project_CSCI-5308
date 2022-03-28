8c67a90e5483651009583560a1d05e359b62ef9e
Ayush Verma <ay322266@dal.ca>
2022-03-28 16:18:01 -0300

    Added tests for serializer Class.

--- password_vault_backend/users/tests.py
+++ password_vault_backend/users/tests.py
@@ -12,22 +12,33 @@ class UserProfileAbstractSerializerTestCase(TestCase):
     @author: Ayush Verma <ayush.verma@dal.ca>
     """
 
+
+    def test_validate_name_with_correct_arguments(self):
+        instantiated_profile_class = UserProfileAbstractSerializer()
+        t = instantiated_profile_class.validate_name("Ayush Verma")
+        self.assertEqual(t, "Ayush Verma")
+
+    def test_validate_name_with_incorrect_arguments(self):
+        instantiated_profile_class = UserProfileAbstractSerializer()
+        t = instantiated_profile_class.validate_name("Ayush Verma@$%&*($*#^#&$")
+        self.assertNotEqual(t, "Ayush Verma")
+
     def test_validate_first_name_with_correct_argument(self):
         instantiated_profile_class = UserProfileAbstractSerializer()
-        t = instantiated_profile_class.validate_name("Ayush")
+        t = instantiated_profile_class.validate_first_name("Ayush")
         self.assertEqual(t, "Ayush")
 
     def test_validate_first_name_with_incorrect_argument(self):
         instantiated_profile_class = UserProfileAbstractSerializer()
-        t = instantiated_profile_class.validate_name("Ayush@")
+        t = instantiated_profile_class.validate_first_name("Ayush@")
         self.assertNotEqual(t, "Ayush")
 
     def test_validate_last_name_with_correct_argument(self):
         instantiated_profile_class = UserProfileAbstractSerializer()
-        t = instantiated_profile_class.validate_name("Verma")
+        t = instantiated_profile_class.validate_last_name("Verma")
         self.assertEqual(t, "Verma")
 
     def test_validate_last_name_with_incorrect_argument(self):
         instantiated_profile_class = UserProfileAbstractSerializer()
-        t = instantiated_profile_class.validate_name("Verma@")
+        t = instantiated_profile_class.validate_last_name("Verma@")
         self.assertNotEqual(t, "Verma")
