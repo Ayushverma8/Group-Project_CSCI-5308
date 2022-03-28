ef88cb0662da7823a4b208296a39d1e6c0ad4b3a
Ayush Verma <ay322266@dal.ca>
2022-03-28 15:51:14 -0300

    Added test cases for the Serializer Class methods.

---
+++ password_vault_backend/users/tests.py
@@ -0,0 +1,33 @@
+from unittest.mock import Mock
+
+from django.test import TestCase
+
+from .serializers import *
+
+
+class UserProfileAbstractSerializerTestCase(TestCase):
+    """
+    Testing the common serializer to verify the input data
+
+    @author: Ayush Verma <ayush.verma@dal.ca>
+    """
+
+    def test_validate_first_name_with_correct_argument(self):
+        instantiated_profile_class = UserProfileAbstractSerializer()
+        t = instantiated_profile_class.validate_name("Ayush")
+        self.assertEqual(t, "Ayush")
+
+    def test_validate_first_name_with_incorrect_argument(self):
+        instantiated_profile_class = UserProfileAbstractSerializer()
+        t = instantiated_profile_class.validate_name("Ayush@")
+        self.assertNotEqual(t, "Ayush")
+
+    def test_validate_last_name_with_correct_argument(self):
+        instantiated_profile_class = UserProfileAbstractSerializer()
+        t = instantiated_profile_class.validate_name("Verma")
+        self.assertEqual(t, "Verma")
+
+    def test_validate_last_name_with_incorrect_argument(self):
+        instantiated_profile_class = UserProfileAbstractSerializer()
+        t = instantiated_profile_class.validate_name("Verma@")
+        self.assertNotEqual(t, "Verma")
