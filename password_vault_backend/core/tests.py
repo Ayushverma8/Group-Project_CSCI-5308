08b8ab5be703465e2bb03fde8a977d9d5934a4e9
Ayush Verma <ay322266@dal.ca>
2022-03-29 14:37:18 -0300

    Added tests for helper class functionalities

--- password_vault_backend/core/tests.py
+++ password_vault_backend/core/tests.py
@@ -1,3 +1,32 @@
+from unittest.mock import MagicMock
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 # Create your tests here.
+from .helpers import *
+
+
+class HelperMethodsTest(TestCase):
+    """
+    Testing the core helper methods
+
+    @author: Ayush Verma <ayush.verma@dal.ca>
+    """
+
+    def test_send_email_functionality_success(self):
+        email_address_to = "ayush.verma@dal.ca"
+        subject = "Hello from other side"
+        template_name = "test_template"
+        context = {"name": "Ayush Verma", "subject": "ASDC", "type": "Project"}
+        send_email = MagicMock(return_value=True)
+        send_test_email = send_email(template_name, context, subject, email_address_to)
+        self.assertEqual(send_test_email, True)
+
+    def test_send_email_functionality_failure(self):
+        email_address_to = "ayush.verma@dal.ca"
+        subject = "Hello from other side"
+        context = {"name": "Ayush Verma", "subject": "ASDC", "type": "Project"}
+        send_email = MagicMock(side_effect=ValidationError('Template name not provided '
+                                                           'with send email'))
+        self.assertRaises(ValidationError, send_email)
