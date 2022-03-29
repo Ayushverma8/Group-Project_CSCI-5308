a65e4506facb2fe44fe5e8bd756d274319df2495
Ayush Verma <ay322266@dal.ca>
2022-03-29 16:08:29 -0300

    Added unittest for ToDo functionalities

--- password_vault_backend/todo/tests.py
+++ password_vault_backend/todo/tests.py
@@ -1,3 +1,41 @@
+from unittest.mock import MagicMock
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from .serializers import *
+
+instantianted_todo_class = ToDoSerializer()
+
+
+class ToDoSerializerTest(TestCase):
+    """
+    Testing ToDo serializer to verify the task generation
+
+    @author: Ayush Verma <ayush.verma@dal.ca>
+    """
+
+    def test_validate_title_correct(self):
+        actual = instantianted_todo_class.validate_title("  Hello Hoomans   ")
+        expexted = "Hello Hoomans"
+        self.assertEqual(actual, expexted)
+
+    def test_create_success(self):
+        serializer_object = ({
+            'first_name': 'Ayush',
+            'last_name': 'Verma',
+            'request': 'ayush.verma@dal.ca',
+            'created_by': 'Ayush'
+        })
+        instantianted_todo_class.create = MagicMock(return_value=serializer_object)
+        self.assertEqual(instantianted_todo_class.create(serializer_object), serializer_object)
+
+    def test_create_failure(self):
+        serializer_object = ({
+            'first_name': 'Ayush',
+            'last_name': 'Verma',
+            'request': 'ayush.verma@dal.ca',
+            'created_by': 'Ayush'
+        })
+        instantianted_todo_class.create = MagicMock(side_effect=ValidationError('Task not created successfully'))
+        self.assertRaises(ValidationError, instantianted_todo_class.create, serializer_object)
