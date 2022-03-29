33540cc5f9183ec09c9fda83da08965e69e33adb
Ayush Verma <ay322266@dal.ca>
2022-03-28 23:14:23 -0300

    Testing gitlab's running mechanism

--- password_vault_backend/notes/tests.py
+++ password_vault_backend/notes/tests.py
@@ -1,19 +1,5 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from .serializers import *
-from unittest.mock import MagicMock
-
-instantiated_note_serializer = NoteSerializer()
-
-
-class NoteSerializerTestCase(TestCase):
-
-    def test_create_successful(self):
-        serializer_object = ({
-            'id': 43333,
-            'title': 'Laundry at 6pm',
-            'text': 'Get loonies and do the laundry at 6pm',
-            'last_name' : 'Verma'
-        })
-        instantiated_note_serializer.create = MagicMock(side_effect=ValidationError("This should be same as password"))
-        self.assertRaises(instantiated_note_serializer.create(), serializer_object)
+from unittest.mock import MagicMock, Mock
+from unittest.mock import patch
