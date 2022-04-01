7669dfd6a7d2042e85a24f8f41f8c253ff41af26
Shalin Hasanbhai Awadiya <sh290595@dal.ca>
2022-04-01 18:02:22 -0300

    Added tests for AES Cipher

--- password_vault_backend/vault/tests.py
+++ password_vault_backend/vault/tests.py
@@ -15,7 +15,7 @@ encrypted_string = pivot.encrypt(string_to_be_encrypted)
 class AESCipherTestCase(TestCase):
     """
     Testing the serializer for Encryption
-
+    @auhor: Shalin Awadiya <sh290595@dal.ca> and
     @author: Ayush Verma <ayush.verma@dal.ca>
     """
 
@@ -32,4 +32,23 @@ class AESCipherTestCase(TestCase):
         # Passing the encrypted content to the decryption function
         decrypted_message = pivot.decrypt(encrypted_string)
         self.assertNotEqual(decrypted_message, "Some random string")
+
+    def test_encryption_length(self):
+        # Confirm if the encrypted results in 256 SHA hash
+        self.assertNotEqual(len(encrypted_string) % 64, 0);
+
+    def test_encryption_return_type(self):
+        # Confirm if the encrypted ciphertext is in string and not in AES bytes
+        returnError = False
+        if isinstance(encrypted_string, bytes):
+            returnError = True
+        self.assertEqual(returnError, False)
+
+    def test_decryption_return_type(self):
+        # Confirm if the decrypted plaintext is in string and not in AES bytes
+        decrypted_string = pivot.decrypt(encrypted_string)
+        returnError = False
+        if isinstance(decrypted_string, bytes):
+            returnError = True
+        self.assertEqual(returnError, False)
 
